e162145488059e49164598e2ce67af19b274ea51
batuyamanturk <yamanturkbatuhan@gmail.com>
2022-05-20 22:19:40 +0300

    Form created in admin panel to ınsert data to database

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.adminbase')
+
+@section('title', 'Add Category')
+
+@section('content')
+
+    <!--main content start-->
+    <section id="main-content">
+        <section class="wrapper site-min-height">
+            <h3><i class="fa fa-angle-right"></i> Add Category</h3>
+            <div class="row mt">
+                <div class="col-lg-12">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            Category Elements
+                        </div>
+                        <div class="panel-body">
+                            <form role="form" method="post" action="/admin/category/store">
+                                @csrf
+                                <div class="form-group">
+                                    <label>Title</label>
+                                    <input class="form-control" type="text" name="title" placeholder="Title" required>
+                                </div>
+                                <div class="form-group">
+                                    <label>Keywords</label>
+                                    <input class="form-control" type="text" name="keywords" placeholder="Keywords">
+                                </div>
+                                <div class="form-group">
+                                    <label>Description</label>
+                                    <input class="form-control" type="text" name="description" placeholder="Description">
+                                </div>
+                                <div class="form-group">
+                                    <label>Image</label>
+                                    <input type="file" name="fileToUpload" id="fileToUpload">
+                                </div>
+                                <div class="form-group">
+                                    <label>Status</label>
+                                    <select class="form-control" name="status">
+                                        <option>True</option>
+                                        <option>False</option>
+                                    </select>
+                                </div>
+
+                                <button type="submit" class="btn btn-info">Save </button>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section><! --/wrapper -->
+    </section><!-- /MAIN CONTENT -->
+
+    <!--main content end-->
+@endsection
---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.adminbase')
+
+@section('title', 'Category List')
+
+@section('content')
+
+    <!--main content start-->
+    <section id="main-content">
+        <section class="wrapper site-min-height">
+            <h3><i class="fa fa-angle-right"></i> Category List</h3>
+            <div class="row mt">
+                <div class="col-lg-12">
+                    <p>Content</p>
+                </div>
+            </div>
+
+        </section><! --/wrapper -->
+    </section><!-- /MAIN CONTENT -->
+
+    <!--main content end-->
+@endsection
