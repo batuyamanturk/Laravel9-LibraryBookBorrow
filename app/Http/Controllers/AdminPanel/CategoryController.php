71a2a5df6b8771dec352c30db34d402e91d2723f
batuyamanturk <yamanturkbatuhan@gmail.com>
2022-05-21 00:52:01 +0300

    Category update date to database

--- app/Http/Controllers/AdminPanel/CategoryController.php
+++ app/Http/Controllers/AdminPanel/CategoryController.php
@@ -42,7 +42,7 @@ class  CategoryController extends Controller
     public function store(Request $request)
     {
         $data= new Category();
-        $data->parent_id = 0;
+        $data->parent_id =1;
         $data->title = $request->title;
         $data->keywords = $request->keywords;
         $data->description = $request->description;
@@ -68,9 +68,12 @@ class  CategoryController extends Controller
      * @param  \App\Models\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function edit(Category $category)
+    public function edit(Category $category,$id)
     {
-        //
+        $data = Category::find($id);
+        return view('admin.category.edit',[
+            'data'=> $data
+        ]);
     }
 
     /**
@@ -80,9 +83,15 @@ class  CategoryController extends Controller
      * @param  \App\Models\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Category $category)
+    public function update(Request $request, Category $category,$id)
     {
-        //
+        $data = Category::find($id);
+        $data->title = $request->title;
+        $data->keywords = $request->keywords;
+        $data->description = $request->description;
+        $data->status = $request->status;
+        $data->save();
+        return redirect('admin/category');
     }
 
     /**
---
+++ resources/views/admin/category/edit.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.adminbase')
+
+@section('title', 'Edit Category : '.$data->title)
+
+@section('content')
+
+    <!--main content start-->
+    <section id="main-content">
+        <section class="wrapper site-min-height">
+            <h3><i class="fa fa-angle-right"></i> Edit Category {{$data->title}}</h3>
+            <div class="row mt">
+                <div class="col-lg-12">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            Category Elements
+                        </div>
+                        <div class="panel-body">
+                            <form role="form" method="post" action="/admin/category/update/{{$data->id}}">
+                                @csrf
+                                <div class="form-group">
+                                    <label>Title</label>
+                                    <input class="form-control" type="text" name="title" value="{{$data->title}}" required>
+                                </div>
+                                <div class="form-group">
+                                    <label>Keywords</label>
+                                    <input class="form-control" type="text" name="keywords" value="{{$data->keywords}}">
+                                </div>
+                                <div class="form-group">
+                                    <label>Description</label>
+                                    <input class="form-control" type="text" name="description" value="{{$data->description}}">
+                                </div>
+                                <div class="form-group">
+                                    <label>Image</label>
+                                    <input type="file" name="fileToUpload" id="fileToUpload">
+                                </div>
+                                <div class="form-group">
+                                    <label>Status</label>
+                                    <select class="form-control" name="status">
+                                        <option selected>{{$data->status}}</option>
+                                        <option>True</option>
+                                        <option>False</option>
+                                    </select>
+                                </div>
+
+                                <button type="submit" class="btn btn-info">Update Data</button>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section><! --/wrapper -->
+    </section><!-- /MAIN CONTENT -->
+
+    <!--main content end-->
+@endsection
