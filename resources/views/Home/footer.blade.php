bba2a9e748da3c1d82506c7d4f907f8ec652a29a
batuyamanturk <yamanturkbatuhan@gmail.com>
2022-05-20 17:32:24 +0300

    Web Template Implementation

---
+++ resources/views/Home/footer.blade.php
@@ -0,0 +1,40 @@
+<footer>
+    <div class="footer">
+        <div class="container">
+            <div class="row pdn-top-30">
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                    <div class="Follow">
+                        <h3>Follow Us</h3>
+                    </div>
+                    <ul class="location_icon">
+                        <li> <a href="#"><img src="{{asset('assets')}}/icon/facebook.png"></a></li>
+                        <li> <a href="#"><img src="{{asset('assets')}}/icon/Twitter.png"></a></li>
+                        <li> <a href="#"><img src="{{asset('assets')}}/icon/linkedin.png"></a></li>
+                        <li> <a href="#"><img src="{{asset('assets')}}/icon/instagram.png"></a></li>
+                    </ul>
+                </div>
+                <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
+                    <div class="Follow">
+                        <h3>Newsletter</h3>
+                    </div>
+                    <input class="Newsletter" placeholder="Enter your email" type="Enter your email">
+                    <button class="Subscribe">Subscribe</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        <div class="container">
+            <p>Copyright 2019 All Right Reserved By <a href="https://html.design/">Free html Templates</a></p>
+        </div>
+    </div>
+    <!-- Javascript files-->
+    <script src="{{asset('assets')}}/js/jquery.min.js"></script>
+    <script src="{{asset('assets')}}/js/popper.min.js"></script>
+    <script src="{{asset('assets')}}/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('assets')}}/js/jquery-3.0.0.min.js"></script>
+    <script src="{{asset('assets')}}/js/plugin.js"></script>
+    <!-- sidebar -->
+    <script src="{{asset('assets')}}/js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="{{asset('assets')}}/js/custom.js"></script>
+</footer>
--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -1,19 +1,45 @@
 <html>
 <head>
-    <title>App Name - @yield('title')</title>
-    @yield('head')
+    <!-- basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- mobile metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <!-- site metas -->
+    <title>@yield("title")</title>
+
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/bootstrap.min.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/style.css">
+    <!-- Responsive-->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/responsive.css">
+    <!-- fevicon -->
+    <link rel="icon" href="images/fevicon.png" type="image/gif" />
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/jquery.mCustomScrollbar.min.css">
+    <!-- Tweaks for older IEs-->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
 </head>
 <body>
-<h1>HEADER</h1>
+@include("home.header")
 
-@section('sidebar')
-    This is the master sidebar.
+
+@section('slider')
+    @include("home.slider")
 @show
 
-<div class="container">
-    @yield('content')
-</div>
-<h1>FOOTER</h1>
+
+@yield('content')
+
+@include("home.footer")
+
 @yield('foot')
 </body>
 </html>
