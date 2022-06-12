e75655f4aefcbfcac2b06ad8e14c299b6a9d35ff
batuyamanturk <yamanturkbatuhan@gmail.com>
2022-06-12 20:00:41 +0300

    Check admin user Permission for Admin Panel on Middleware

--- routes/web.php
+++ routes/web.php
@@ -36,10 +36,10 @@ Route::get('/contact',[HomeController::class,'contact'])->name('contact');
 Route::post('/storemessage',[HomeController::class,'storemessage'])->name('storemessage');
 Route::post('/storecomment',[HomeController::class,'storecomment'])->name('storecomment');
 Route::get('/faq',[HomeController::class,'faq'])->name('faq');
-Route::view('/loginuser','home.login');
-Route::view('/registeruser','home.register');
+Route::view('/loginuser','home.login')->name('loginuser');
+Route::view('/registeruser','home.register')->name('registeruser');
 Route::get('/logoutuser',[HomeController::class,'logout'])->name('logoutuser');
-Route::view('/loginadmin','admin.login');
+Route::view('/loginadmin','admin.login')->name('loginadmin');
 Route::post('/loginadmincheck',[HomeController::class,'loginadmincheck'])->name('loginadmincheck');
 
 
@@ -59,7 +59,7 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 
 //***************************ADMIN PANEL ROUTES ************************************************************************
-Route::prefix('admin')->name('admin.')->group(function () {
+Route::middleware('admin')->prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [AdminHomeController::class, 'index'])->name('index');
 //********************************GENERAL ROUTES ****************************************************************
     Route::get('/settings', [AdminHomeController::class, 'settings'])->name('settings');
